b07970ea9c219405b3a449e287e1738cc9031416
Jennifer Holcomb <jenniferlholcomb@gmail.com>
2024-08-30 17:25:59 -0700

    WIP pantry pro layout

--- src/components/PantryPro.jsx
+++ src/components/PantryPro.jsx
@@ -21,15 +21,16 @@ function PantryPro() {
           : null
         }
         <div className={styles.topBar}>
-
         <div className={styles.logo}>
           <img src={invLogo} alt="top banner, graphic with food" />
         </div>
         <div className={styles.banner}>
           <img src={bannerTop} alt="top banner, graphic with food" />  
         </div>
+
+
         </div>        
-        <div className={styles.section1}>
+        <div className={styles.sectionIntro}>
           <h1 className={styles.intro}>Inventory & Invoice Management Application</h1>              
           <svg xmlns="http://www.w3.org/2000/svg" width="532" height="2" viewBox="0 0 532 2" fill="none">
             <path d="M531 2C531.552 2 532 1.55228 532 1C532 0.447715 531.552 0 531 0V2ZM0 2H531V0H0V2Z" fill="#001B08"/>
@@ -44,10 +45,44 @@ function PantryPro() {
           <img className={styles.imgInv} src={ex2} alt="app inventory listing page" />
           <img className={styles.imgInv} src={ex3} alt="app product counting page" />
         </div>
+
+
+        <div className={styles.section1}>
+          <div className={styles.oneInfo}>
+            <div className={styles.numberInv}>01.</div>
+            <h2 className={styles.headersInv}>The Problem</h2>
+          </div>
+          <div className={styles.line}>
+            <svg className={styles.line} xmlns="http://www.w3.org/2000/svg" width="1728" height="2" viewBox="0 0 1728 2" fill="none">
+              <path d="M1743 2C1743.55 2 1744 1.55228 1744 1C1744 0.447715 1743.55 0 1743 0V2ZM-18 2H1743V0H-18V2Z" fill="#001B08"/>
+            </svg>
+          </div>
+          
+
+            <p className={styles.blurb11}>So how to address these two challenges,</p>
+            <p className={styles.blurb12}>1. With modern technology, users no longer need to manually enter line items from invoices. AI can facilitate managers with scanning and categorizing data into fields that can be edited and cataloged. Based on interviews with restaurant managers, invoice data entry is time consuming and suspect of human error. A system to alleviate these pain points is valid in creating trusted and productive inventory management. </p>
+            <p className={styles.blurb13}>2. Who likes counting, over and over again? Keeping accurate counts of inventory on hand is a necessary part of weekly costing. For small restaurants, and food carts, profits can drastically swing from week to week. A system designed to aid managers in quick counting of goods, and reports itemizing what needs a restock, are both tools that could ease the daily rigor of owning or operating a small restaurant.</p>
+          
+
+          </div>
+          <div className={styles.oneCarousel}>
+            <div className={styles.carouselContent}>
+
+            </div>
+        </div>
+            <div className={styles.twoBlurb}>
+            <h2 className={styles.headersInv}>The Problem</h2>
+            <p className={styles.blurb1}>2. Who likes counting, over and over again? Keeping accurate counts of inventory on hand is a necessary part of weekly costing. For small restaurants, and food carts, profits can drastically swing from week to week. A system designed to aid managers in quick counting of goods, and reports itemizing what needs a restock, are both tools that could ease the daily rigor of owning or operating a small restaurant.</p>
+            </div>
       </div>
-      
-      <div className={styles.bubble1}></div>
-      <div className={styles.bubble1}></div>
+
+
+      <div className={styles.bubbles}>
+        <div className={styles.bubble1}></div>
+        <div className={styles.bubble2}></div>
+        <div className={styles.bubble2}></div>
+        
+      </div>        
     </>
   );
 }
