9494aab10a2b8cea395f7295e3d34014dc50e932
Jennifer Holcomb <jenniferlholcomb@gmail.com>
2024-09-01 14:16:43 -0700

    WIP pantry pro layout

--- src/components/PantryPro.jsx
+++ src/components/PantryPro.jsx
@@ -8,6 +8,11 @@ import bannerTop from "./../img/bannerImageWide.webp";
 import ex1 from "./../img/InvEx1.webp";
 import ex2 from "./../img/InvEx2.webp";
 import ex3 from "./../img/InvEx3.webp";
+import stMp from "./../img/InventorySitemap.svg";
+import hm1 from "./../img/Homepage1.webp";
+import hm2 from "./../img/Homepage2.webp";
+import hm3 from "./../img/Homepage3.webp";
+import hm4 from "./../img/Homepage4.webp";
 
 function PantryPro() {
   const [ isMobile, isDesktop ] = useResize();
@@ -28,7 +33,6 @@ function PantryPro() {
           <img src={bannerTop} alt="top banner, graphic with food" />  
         </div>
 
-
         </div>        
         <div className={styles.sectionIntro}>
           <h1 className={styles.intro}>Inventory & Invoice Management Application</h1>              
@@ -41,40 +45,87 @@ function PantryPro() {
           </div>
         </div>
         <div className={styles.exShotInv}>
-          <img src={ex1} alt="app landing page" className={styles.imgInv} />
+          <img className={styles.imgInv} src={ex1} alt="app landing page" />
           <img className={styles.imgInv} src={ex2} alt="app inventory listing page" />
           <img className={styles.imgInv} src={ex3} alt="app product counting page" />
         </div>
 
-
         <div className={styles.section1}>
           <div className={styles.oneInfo}>
             <div className={styles.numberInv}>01.</div>
-            <h2 className={styles.headersInv}>The Problem</h2>
+            <h2 className={styles.headersInv} id={styles.headers}>The Problem</h2>
           </div>
           <div className={styles.line}>
             <svg className={styles.line} xmlns="http://www.w3.org/2000/svg" width="1728" height="2" viewBox="0 0 1728 2" fill="none">
               <path d="M1743 2C1743.55 2 1744 1.55228 1744 1C1744 0.447715 1743.55 0 1743 0V2ZM-18 2H1743V0H-18V2Z" fill="#001B08"/>
             </svg>
-          </div>
-          
-
-            <p className={styles.blurb11}>So how to address these two challenges,</p>
-            <p className={styles.blurb12}>1. With modern technology, users no longer need to manually enter line items from invoices. AI can facilitate managers with scanning and categorizing data into fields that can be edited and cataloged. Based on interviews with restaurant managers, invoice data entry is time consuming and suspect of human error. A system to alleviate these pain points is valid in creating trusted and productive inventory management. </p>
-            <p className={styles.blurb13}>2. Who likes counting, over and over again? Keeping accurate counts of inventory on hand is a necessary part of weekly costing. For small restaurants, and food carts, profits can drastically swing from week to week. A system designed to aid managers in quick counting of goods, and reports itemizing what needs a restock, are both tools that could ease the daily rigor of owning or operating a small restaurant.</p>
-          
-
-          </div>
-          <div className={styles.oneCarousel}>
-            <div className={styles.carouselContent}>
-
-            </div>
+          </div>          
         </div>
-            <div className={styles.twoBlurb}>
-            <h2 className={styles.headersInv}>The Problem</h2>
-            <p className={styles.blurb1}>2. Who likes counting, over and over again? Keeping accurate counts of inventory on hand is a necessary part of weekly costing. For small restaurants, and food carts, profits can drastically swing from week to week. A system designed to aid managers in quick counting of goods, and reports itemizing what needs a restock, are both tools that could ease the daily rigor of owning or operating a small restaurant.</p>
-            </div>
+        <p id={styles.blurb1} className={styles.blOne}>How to address these two challenges?</p>
+        <p id={styles.blurb1} className={styles.blTwo}>1. With modern technology, users no longer need to manually enter line items from invoices. AI can facilitate managers with scanning and categorizing data into fields that can be edited and cataloged. Based on interviews with restaurant managers, invoice data entry is time consuming and suspect of human error. A system to alleviate these pain points is valid in creating trusted and productive inventory management. </p>
+        <p id={styles.blurb1} className={styles.blThree}>2. Who likes counting, over and over again? Keeping accurate counts of inventory on hand is a necessary part of weekly costing. For small restaurants, and food carts, profits can drastically swing from week to week. A system designed to aid managers in quick counting of goods, and reports itemizing what needs a restock, are both tools that could ease the daily rigor of owning or operating a small restaurant.</p>         
+        <div className={styles.oneCarousel}>
+          <div className={styles.carouselContent}>
+        </div>
+        </div>
+        <div className={styles.twoBlurb}>
+          <h2 className={styles.headersInv} id={styles.userResHead}>User Research</h2>
+          <svg xmlns="http://www.w3.org/2000/svg" width="677" height="2" viewBox="0 0 677 2" fill="none">
+            <path d="M697 2.00006C697.552 2.00006 698 1.55235 698 1.00006C698 0.447776 697.552 6.0982e-05 697 6.09337e-05L697 2.00006ZM-8.74228e-08 2L697 2.00006L697 6.09337e-05L8.74228e-08 0L-8.74228e-08 2Z" fill="#001B08"/>
+          </svg>
+          <p id={styles.blurb1} className={styles.blFour}>2. Who likes counting, over and over again? Keeping accurate counts of inventory on hand is a necessary part of weekly costing. For small restaurants, and food carts, profits can drastically swing from week to week. A system designed to aid managers in quick counting of goods, and reports itemizing what needs a restock, are both tools that could ease the daily rigor of owning or operating a small restaurant.</p>
+        </div>
+
+        <div className={`${styles.section1} ${styles.problem}`}>
+          <div className={styles.oneInfo}>
+            <div className={styles.numberInv}>02.</div>
+            <h2 className={styles.headersInv} id={styles.headers}>Solution</h2>
+          </div>
+          <svg className={styles.line} xmlns="http://www.w3.org/2000/svg" width="1728" height="2" viewBox="0 0 1728 2" fill="none">
+            <path d="M1743 2C1743.55 2 1744 1.55228 1744 1C1744 0.447715 1743.55 0 1743 0V2ZM-18 2H1743V0H-18V2Z" fill="#001B08"/>
+          </svg>
+        </div>
+        <p id={styles.blurb1} className={styles.blOne}>2. Who likes counting, over and over again? Keeping accurate counts of inventory on hand is a necessary part of weekly costing. For small restaurants, and food carts, profits can drastically swing from week to week. A system designed to aid managers in quick counting of goods, and reports itemizing what needs a restock, are both tools that could ease the daily rigor of owning or operating a small restaurant.</p>
+        <div className={styles.site}>
+          <img className={styles.siteMap} src={stMp} alt="sitemap illustration" />
+        </div>
+
+        <div className={styles.section3}>
+          <img className={styles.img3} src={ex1} alt="app landing page"  />
+          <img className={styles.img3} src={ex2} alt="app inventory listing page" />
+        </div>
+
+        <div className={`${styles.section4Img} ${styles.section4}`}>
+          <img src={hm1} alt="landing page iteration 1" />
+          <img src={hm2} alt="landing page iteration 2" />
+          <img src={hm3} alt="landing page iteration 3" />
+          <img src={hm4} alt="landing page iteration 4" />
+        </div>
+        <div className={styles.twoBlurb}>
+          <h2 className={styles.headersInv} id={styles.usaStdy}>Usability Study</h2>
+          <svg xmlns="http://www.w3.org/2000/svg" width="677" height="2" viewBox="0 0 677 2" fill="none">
+            <path d="M697 2.00006C697.552 2.00006 698 1.55235 698 1.00006C698 0.447776 697.552 6.0982e-05 697 6.09337e-05L697 2.00006ZM-8.74228e-08 2L697 2.00006L697 6.09337e-05L8.74228e-08 0L-8.74228e-08 2Z" fill="#001B08"/>
+          </svg>
+          <p id={styles.blurb1} >2. Who likes counting, over and over again? Keeping accurate counts of inventory on hand is a necessary part of weekly costing. For small restaurants, and food carts, profits can drastically swing from week to week. A system designed to aid managers in quick counting of goods, and reports itemizing what needs a restock, are both tools that could ease the daily rigor of owning or operating a small restaurant.</p>
+        </div>
+        <div className={`${styles.section4Img} ${styles.section4B}`}>
+          <img src={hm1} alt="landing page iteration 1" />
+          <img src={hm2} alt="landing page iteration 2" />
+          <img src={hm3} alt="landing page iteration 3" />
+        </div>
+        <img className={`${styles.sec4LastImg} ${styles.section4B}`} src={hm4} alt="landing page iteration 4" />
+
+        <h2 className={styles.headersInv} id={styles.desDetails}>Design Details</h2>
+        <h2 className={styles.headersInv} id={styles.desKit}>Design Kit</h2>
+        <svg className={styles.line} id={styles.lineDes} xmlns="http://www.w3.org/2000/svg" width="1728" height="2" viewBox="0 0 1728 2" fill="none">
+          <path d="M1743 2C1743.55 2 1744 1.55228 1744 1C1744 0.447715 1743.55 0 1743 0V2ZM-18 2H1743V0H-18V2Z" fill="#001B08"/>
+        </svg>
+        <div className={styles.section5}>
+          <p id={styles.blurb1}>2. Who likes counting, over and over again? Keeping accurate counts of inventory on hand is a necessary part of weekly costing. For small restaurants, and food carts, profits can drastically swing from week to week. A system designed to aid managers in quick counting of goods, and reports itemizing what needs a restock, are both tools that could ease the daily rigor of owning or operating a small restaurant.</p>
+        </div>
+
       </div>
+
 
 
       <div className={styles.bubbles}>
