5162e749734b2cdace22b327cf15c0dc412d99fd
Jennifer Holcomb <jenniferlholcomb@gmail.com>
2024-09-17 11:10:27 -0700

    update blurbs

--- src/components/PantryPro.jsx
+++ src/components/PantryPro.jsx
@@ -56,16 +56,20 @@ function PantryPro() {
 
         </div>        
         <div className={styles.sectionIntro}>
-          <h1 className={styles.intro}>Inventory & Invoice Management Application</h1>              
+          <div className={styles.info}>
+            <h3 className={styles.infoYear}>
+              Pantry Pro 
+              <br />
+              Conceptual Mobile App
+              <br />
+              2024
+            </h3>
+          </div> 
+          <p className={`${styles.infoBlurb} ${styles.info}`} id={styles.summary}>Improving inventory and invoice management for restaurant owners and operators.</p>            
           <svg xmlns="http://www.w3.org/2000/svg" width="532" height="2" viewBox="0 0 532 2" fill="none">
             <path d="M531 2C531.552 2 532 1.55228 532 1C532 0.447715 531.552 0 531 0V2ZM0 2H531V0H0V2Z" fill="#001B08"/>
           </svg>
-          <div className={styles.info}>
-            <h3 className={styles.infoYear}>
-              Case Study 
-              <br />
-              <span className={styles.year}>2024</span>
-            </h3>                
+          <div className={styles.info}>               
             <p className={styles.infoBlurb}>Two challenges in the realm of restaurant inventory management; the slow process of entering new invoice data, and the tedious job of counting goods. This app is conceptualized to give the user power to upload invoices using AI technology, and a design to prevent task fatigue while counting items. In addition to being a streamlined application for managing and categorizing inventory.</p>
           </div>
         </div>
@@ -86,9 +90,9 @@ function PantryPro() {
             </svg>
           </div>          
         </div>
-        <p id={styles.blurb1} className={styles.blOne}>How to address these two challenges?</p>
-        <p id={styles.blurb1} className={styles.blTwo}>1. With modern technology, users no longer need to manually enter line items from invoices. AI can facilitate managers with scanning and categorizing data into fields that can be edited and cataloged. Based on interviews with restaurant managers, invoice data entry is time consuming and suspect of human error. A system to alleviate these pain points is valid in creating trusted and productive inventory management. </p>
-        <p id={styles.blurb1} className={styles.blThree}>2. Who likes counting, over and over again? Keeping accurate counts of inventory on hand is a necessary part of weekly costing. For small restaurants, and food carts, profits can drastically swing from week to week. A system designed to aid managers in quick counting of goods, and reports itemizing what needs a restock, are both tools that could ease the daily rigor of owning or operating a small restaurant.</p>         
+        <p id={styles.blurb1} className={styles.blOne}>How to improve the experience of managing inventory for restaurants and food carts?  After research and interviews, I was able to outline a few inconveniences of overseeing the ebb and flow of inventory, and pain points of other inventory management apps.</p>
+        <p id={styles.blurb1} className={styles.blTwo}>Overwhelmingly, updating logs of current inventory was voiced by managers as a monotonous and dreaded responsibility.  Small restaurant owners cited the chore as not being a priority, with time better spent on other business dealings.</p>
+        <p id={styles.blurb1} className={styles.blThree}>Though, a common grievance among business owners is rising cost of goods and labor, both contributing to higher daily operational expenses.  All recognize a necessity for keeping accurate books, and desire for an improved system to manage inventory.</p>         
   
         <Slides slideIndex={carouselIndex} handleCircleClick={handleCircleClick}/>
        
@@ -97,7 +101,9 @@ function PantryPro() {
           <svg xmlns="http://www.w3.org/2000/svg" width="677" height="2" viewBox="0 0 677 2" fill="none">
             <path d="M697 2.00006C697.552 2.00006 698 1.55235 698 1.00006C698 0.447776 697.552 6.0982e-05 697 6.09337e-05L697 2.00006ZM-8.74228e-08 2L697 2.00006L697 6.09337e-05L8.74228e-08 0L-8.74228e-08 2Z" fill="#001B08"/>
           </svg>
-          <p id={styles.blurb1} className={styles.blFour}>2. Who likes counting, over and over again? Keeping accurate counts of inventory on hand is a necessary part of weekly costing. For small restaurants, and food carts, profits can drastically swing from week to week. A system designed to aid managers in quick counting of goods, and reports itemizing what needs a restock, are both tools that could ease the daily rigor of owning or operating a small restaurant.</p>
+          <p id={styles.blurb1}>The user for this project consists of restaurant, and food truck, owners and operators. They require an app that simplifies how data updates are made when receiving inventory, as well as managing accurate counts of inventory on hand. Additionally, desire for an interface that is easy to navigate, and streamlines product information.</p>
+          
+          <p id={styles.blurb1}>A monthly inventory count helper, ability to upload invoices from photo or pdf, easy access to past reports, auto updates to product quantities, and auto reminders for products needing to be replenished &mdash; all features recommended for this app to be successful.</p>
         </div>
 
         <div className={`${styles.section1} ${styles.problem}`}>
@@ -109,7 +115,7 @@ function PantryPro() {
             <path d="M1743 2C1743.55 2 1744 1.55228 1744 1C1744 0.447715 1743.55 0 1743 0V2ZM-18 2H1743V0H-18V2Z" fill="#001B08"/>
           </svg>
         </div>
-        <p id={styles.blurb1} className={styles.blOne}>2. Who likes counting, over and over again? Keeping accurate counts of inventory on hand is a necessary part of weekly costing. For small restaurants, and food carts, profits can drastically swing from week to week. A system designed to aid managers in quick counting of goods, and reports itemizing what needs a restock, are both tools that could ease the daily rigor of owning or operating a small restaurant.</p>
+        <p id={styles.blurb1} className={styles.blOne}>How to address the challenges of task fatigue when entering invoice data or counting inventory?<br /><br />1. With modern technology, manual data entry can be eliminated. AI can facilitate managers with scanning invoices and categorizing data into fields that can be edited and cataloged. This removes chance for human error, and frees up valuable time. An app invoice reader is necessary for establishing trusted inventory data.<br /><br />2. Who likes counting, over and over again? Keeping accurate counts of inventory on hand is a requirement of weekly costing. For small restaurants, and food carts, profits can drastically change week to week. A system designed to aid managers in quick counting of goods, and reports itemizing what needs a restock, are both tools that could ease the daily rigor of owning or operating a small restaurant.</p>
         <div className={styles.site}>
           <img loading="lazy" className={styles.siteMap} src={stMp} alt="sitemap illustration" />
         </div>
