5eb937f8427809519f58e824cf56f81228caa206
Jennifer Holcomb <jenniferlholcomb@gmail.com>
2025-01-23 13:08:39 -0800

    update home project styling

--- src/components/HomeProject.jsx
+++ src/components/HomeProject.jsx
@@ -21,57 +21,83 @@ function HomeProject(props) {
         </div>
         <div className={styles.cardContainer}>
           <section className={styles.bubbleCard}>
-            <div className={styles.picture}>
-              <Link to={"/projects/pantrypro"} className={styles.linkCardStyle} onClick={projectClick}>
-                <img className={styles.imgMask} src={bsCard} />
-              </Link>
-            </div>
             <div className={styles.bubbleContent}>
               <div className={styles.bubbleTop}>
+                <div className={styles.number}>01.</div>
                 <h3 className={styles.project}>dashboard</h3>
+                {/* <div className={styles.projectArrow}>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="7" height="14" viewBox="0 0 7 14" fill="none">
+                    <path d="M7 7L0 0V14L7 7Z" fill="#001B08"/>
+                  </svg>
+                </div> */}
               </div>
               <div className={styles.bubbleBottom}>
                 <h4 className={styles.jobTag}>Full Stack Development</h4>
                 <h4 className={styles.jobTag}>UX Design</h4>
               </div>
             </div>
+            <div className={styles.picture}>
+              <Link to={"/projects/pantrypro"} className={styles.linkCardStyle} onClick={projectClick}>
+                <img className={styles.imgMask} src={bsCard} />
+              </Link>
+            </div>
           </section>
 
           <section className={styles.bubbleCard}>
-            <div className={styles.picture}>
-              <Link to={"/projects/pantrypro"} className={styles.linkCardStyle} onClick={projectClick}>
-                <img className={styles.imgMask} src={ppCard} />
-              </Link>
-            </div>
             <div className={styles.bubbleContent}>
               <div className={styles.bubbleTop}>
+                <div className={styles.number}>02.</div>
                 <h3 className={styles.project}>case study</h3>
+                {/* <div className={styles.projectArrow}>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="7" height="14" viewBox="0 0 7 14" fill="none">
+                    <path d="M7 7L0 0V14L7 7Z" fill="#001B08"/>
+                  </svg>
+                </div> */}
               </div>
               <div className={styles.bubbleBottom}>
                 <h4 className={styles.jobTag}>UX Design</h4>
                 <h4 className={styles.noStyle}>.</h4>
               </div>
             </div>
+            <div className={styles.picture}>
+              <Link to={"/projects/pantrypro"} className={styles.linkCardStyle} onClick={projectClick}>
+                <img className={styles.imgMask} src={ppCard} />
+              </Link>
+            </div>
           </section>
 
           <section className={styles.bubbleCard}>            
-            <div className={styles.picture}>
-              <Link to={"/projects/pantrypro"} className={styles.linkCardStyle} onClick={projectClick}>
-                <img className={styles.imgMask} src={tbCard} />
-              </Link>
-            </div>
             <div className={styles.bubbleContent}>
               <div className={styles.bubbleTop}>
+                <div className={styles.number}>03.</div>
                 <h3 className={styles.project}>e-commerce website</h3>
+                {/* <div className={styles.projectArrow}>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="7" height="14" viewBox="0 0 7 14" fill="none">
+                    <path d="M7 7L0 0V14L7 7Z" fill="#001B08"/>
+                  </svg>
+                </div> */}
               </div>
               <div className={styles.bubbleBottom}>
                 <h4 className={styles.jobTag}>Front End Development</h4>
                 <h4 className={styles.jobTag}>UX Design</h4>
               </div>
             </div>
+            <div className={styles.picture}>
+              <Link to={"/projects/pantrypro"} className={styles.linkCardStyle} onClick={projectClick}>
+                <img className={styles.imgMask} src={tbCard} />
+              </Link>
+            </div>
           </section>
         </div>
   
+        {/* <div className={styles.vertLine}>
+          {/* <div className={styles.line3}></div> */}
+          {/* <div className={styles.line1}></div>
+          <div className={styles.line1}></div>
+        </div> */}
+        {/* <div className={styles.vertLine2}>
+          <div className={styles.line1}></div>
+        </div>  */}
         {/* {cardArr.map((entry, index) => 
         <div className={styles.projectCard}>
           <h1>{entry}</h1>
--- src/components/PastWork.jsx
+++ src/components/PastWork.jsx
@@ -29,7 +29,7 @@ function PastWork() {
                 <div className={styles.number}>01.</div>
                 <h2 className={styles.headers}><span className={styles.blocked}>Brand Identity</span><span className={styles.blocked}>+ Graphic Design</span></h2>
               </div>
-              <svg xmlns="http://www.w3.org/2000/svg" width="361" height="2" viewBox="0 0 361 2" fill="none">
+              <svg xmlns="http://www.w3.org/2000/svg" width="361" height="1" viewBox="0 0 361 1" fill="none">
                 <path d="M360 2C360.552 2 361 1.55228 361 1C361 0.447715 360.552 0 360 0V2ZM0 2H360V0H0V2Z" fill="#001B08"/>
               </svg>
               <p className={styles.blurb}>My strategy for developing brand identities is to align company culture with client input, work to create cohesive logo and branding across in-store and digital platforms, then elevate consumer brand awareness. Thought is given to interior design, refreshed employee uniforms, stylized menus, and website interfaces. Additionally, a logo-driven merchandise program is created to further strengthen brand recognition.</p>
@@ -43,7 +43,7 @@ function PastWork() {
                 <div className={styles.number}>02.</div>
                 <h2 className={styles.headers}>Logo Design</h2>
               </div>
-                <svg xmlns="http://www.w3.org/2000/svg" width="361" height="2" viewBox="0 0 361 2" fill="none">
+                <svg xmlns="http://www.w3.org/2000/svg" width="361" height="1" viewBox="0 0 361 1" fill="none">
                   <path d="M360 2C360.552 2 361 1.55228 361 1C361 0.447715 360.552 0 360 0V2ZM0 2H360V0H0V2Z" fill="#001B08"/>
                 </svg>
                 <p className={styles.blurb}>Creating custom logo designs is a collaborative effort with the client.  Mashing multiple points of inspiration, colors, font styles, or geographical location; all are factors contributing to the unique artwork. Starting with process sketches, then iterating multiple times over design, before digitizing final art. Continuing to work with the client, a series of color studies is presented to ensure final design is aligned with their brand.  </p>
@@ -57,7 +57,7 @@ function PastWork() {
                 <div className={styles.number}>03.</div>
                 <h2 className={styles.headers}><span className={styles.blocked}>Accessory Design</span><span className={styles.blocked}>+ Founder</span></h2>
               </div>
-                <svg xmlns="http://www.w3.org/2000/svg" width="361" height="2" viewBox="0 0 361 2" fill="none">
+                <svg xmlns="http://www.w3.org/2000/svg" width="361" height="1" viewBox="0 0 361 1" fill="none">
                   <path d="M360 2C360.552 2 361 1.55228 361 1C361 0.447715 360.552 0 360 0V2ZM0 2H360V0H0V2Z" fill="#001B08"/>
                 </svg>
                 <p className={styles.blurb}>I launched a startup leather bag and accessory company, ok Louise. Brand ethos was to reduce waste and maximize yield by pairing cleverly engineered patterns with high quality deadstock leathers. Inspired by the strength and spirit of the Pacific Northwest woman, both past and present, designs were highly functional and built to be worn season after season.</p>
@@ -71,7 +71,7 @@ function PastWork() {
                 <div className={styles.number}>04.</div>
                 <h2 className={styles.headers}><span className={styles.blocked}>Pattern Engineering</span><span className={styles.blocked}>+ Tech Sketching</span></h2>
               </div>
-                <svg xmlns="http://www.w3.org/2000/svg" width="361" height="2" viewBox="0 0 361 2" fill="none">
+                <svg xmlns="http://www.w3.org/2000/svg" width="361" height="1" viewBox="0 0 361 1" fill="none">
                   <path d="M360 2C360.552 2 361 1.55228 361 1C361 0.447715 360.552 0 360 0V2ZM0 2H360V0H0V2Z" fill="#001B08"/>
                 </svg>
                 <p className={styles.blurb}>In addition to founding my own accessory brand, I also partnered with other label founders. My speciality was in technical design and technical packages, as well as digital and flat pattern engineering. Though, I also worked on organizing order of operations and pattern inventories for bag production, prototype construction, and costing.</p>
@@ -85,7 +85,7 @@ function PastWork() {
                 <div className={styles.number}>05.</div>
                 <h2 className={styles.headers}>Teaching</h2>
               </div>
-                <svg xmlns="http://www.w3.org/2000/svg" width="361" height="2" viewBox="0 0 361 2" fill="none">
+                <svg xmlns="http://www.w3.org/2000/svg" width="361" height="1" viewBox="0 0 361 1" fill="none">
                   <path d="M360 2C360.552 2 361 1.55228 361 1C361 0.447715 360.552 0 360 0V2ZM0 2H360V0H0V2Z" fill="#001B08"/>
                 </svg>
                 <p className={styles.blurb}>
