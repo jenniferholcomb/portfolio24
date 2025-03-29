64e426be91f27a18739e5f7726606d2997afaad2
Jennifer Holcomb <jenniferlholcomb@gmail.com>
2025-03-28 17:51:36 -0700

    resume breakpoint edits

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,22 +1,14 @@
 import React from "react";
 
-import useResize from "./hooks/useResize";
-import Header from "./Header";
 import styles from "./Resume.module.scss";
 
 import resume from "/img/resume.webp";
 
 function Resume() {
-  const [ isMobile, isDesktop ] = useResize();
   
   return (
     <>
       <div className={styles.resumeWrapper}>
-        {
-          isMobile ?
-            <Header />
-          : null
-        }
         <h1 className={styles.pageHeader}>RESUME</h1>
         <div className={styles.resumeContainer}>
           <img src={resume} alt="resume" />
