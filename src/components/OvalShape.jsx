3e2e5ae28d273480762e388770a977dd14217f36
Jennifer Holcomb <jenniferlholcomb@gmail.com>
2025-04-08 13:59:31 -0700

    oval edits

--- src/components/OvalShape.jsx
+++ src/components/OvalShape.jsx
@@ -151,7 +151,7 @@ const OvalShape = ({ flipPhoto }) => {
           scale={1.1}
           animate={action ? (isTapped ? "tap" : "rest") : "load"}
           whileHover={"hover"}
-          onTap={() => setIsTapped(true)}
+          // onTap={() => setIsTapped(true)}
           onPointerDown={() => setIsTapped(true)}
           // onAnimationComplete={() => setIsTapped(false)}
         >
