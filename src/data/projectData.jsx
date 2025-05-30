d5a1730acfe6c1913fd1b174a75d3ab8336add52
Jennifer Holcomb <jenniferlholcomb@gmail.com>
2025-05-30 08:48:51 -0700

    update resume

--- src/data/projectData.jsx
+++ src/data/projectData.jsx
@@ -15,6 +15,20 @@ const projectData = [
     technologies: ["React", "CSS", "JavaScript", "Firestore", "API's", "Figma"], 
     description: "Bend In Spoon is a dashboard forecasting tool for small restaurants. It tracks occupancy rates for local short-term rentals, cost of goods fluctuations, and provides a weekly breakdown of labor estimates, past sales, weather, events, holidays, and tourism. The app utilizes APIs for short-term rental availability, holidays, weather, and local events. Designed in Figma and built in React, styled with vanilla CSS, and uses Firestore for data storage. The project originated as my capstone project for Epicodus."
   },
+    {
+    projectName: "Terra Bean Coffee Co.",
+    id: 3,
+    externalLink: true,
+    link: "https://terrabeancoffee.netlify.app",
+    githubLink: "https://github.com/jenniferholcomb/terrabeancoffee",
+    imgCard: "/img/terraBean_previewIMG.webp", 
+    smCard: "/img/terraBeanThumbnailNoClip.webp",
+    projectSubhead: ["ecommerce website"], 
+    projectYear: "2024", 
+    disciplines: ["Front End Development", "UI Design"], 
+    technologies: ["React", "CSS", "JavaScript", "Figma", "Photoshop"], 
+    description: "A conceptual coffee bean roasters ecommerce website, this project was a practice in design, as well as front end programming. Application has full CRUD capability, a shopping cart, navigation bar, and explores movement of UI elements with scrolling. Designed in Figma, with AI images, graphics, and logo manipulated in Photoshop. The origins of this project are from an application prompt for tracking inventory for a small online retailer. Inventory functionality, as well as admin duties, are displayed on index cards."
+  },
   {
     projectName: "Pantry Pro",
     id: 2,
@@ -28,20 +42,6 @@ const projectData = [
     disciplines: ["UX/UI Design"], 
     technologies: ["Figma", "Illustrator", "Photoshop"], 
     description: "This case study conceptualizes an app to streamline restaurant inventory management by addressing two key challenges: the time-consuming nature of manual invoice entry and the tediousness of counting stock. Leveraging AI technology, the app allows for efficient invoice uploads and is designed to minimize task fatigue during item counts. The app also provides a centralized platform for managing and categorizing inventory."
-  },
-  {
-    projectName: "Terra Bean Coffee Co.",
-    id: 3,
-    externalLink: true,
-    link: "https://terrabeancoffee.netlify.app",
-    githubLink: "https://github.com/jenniferholcomb/terrabeancoffee",
-    imgCard: "/img/terraBean_previewIMG.webp", 
-    smCard: "/img/terraBeanThumbnailNoClip.webp",
-    projectSubhead: ["ecommerce website"], 
-    projectYear: "2024", 
-    disciplines: ["Front End Development", "UI Design"], 
-    technologies: ["React", "CSS", "JavaScript", "Figma", "Photoshop"], 
-    description: "A conceptual coffee bean roasters ecommerce website, this project was a practice in design, as well as front end programming. Application has full CRUD capability, a shopping cart, navigation bar, and explores movement of UI elements with scrolling. Designed in Figma, with AI images, graphics, and logo manipulated in Photoshop. The origins of this project are from an application prompt for tracking inventory for a small online retailer. Inventory functionality, as well as admin duties, are displayed on index cards."
   },
   {
     projectName: "Personal Website",
